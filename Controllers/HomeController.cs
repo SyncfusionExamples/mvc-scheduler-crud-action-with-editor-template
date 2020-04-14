0b7ef08c488f34653f67ca8d6082132826fc734f
Rajendran R <rajendranr-5483@users.noreply.github.com>
2020-04-14 18:46:46 +0530

    Files are added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ScheduleSample.Models;
+using Syncfusion.EJ2.Schedule;
+
+namespace ScheduleSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult LoadData(Params param)  // Here we get the Start and End Date and based on that can filter the data and return to Scheduler
+        {
+            DateTime start = param.StartDate;
+            DateTime end = param.EndDate;
+            var data = db.ScheduleEventDatas.Where(app => (app.StartTime >= start && app.StartTime <= end)).ToList();  // Here filtering the events based on the start and end date value.
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        public class Params
+        {
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+        }
+
+        [HttpPost]
+        public JsonResult UpdateData(EditParams param)
+        {
+            if (param.action == "insert" || (param.action == "batch" && param.added != null)) // this block of code will execute while inserting the appointments
+            {
+                var value = (param.action == "insert") ? param.value : param.added[0];
+                int intMax = db.ScheduleEventDatas.ToList().Count > 0 ? db.ScheduleEventDatas.ToList().Max(p => p.Id) : 1;
+                DateTime startTime = Convert.ToDateTime(value.StartTime);
+                DateTime endTime = Convert.ToDateTime(value.EndTime);
+                ScheduleEventData appointment = new ScheduleEventData()
+                {
+                    Id = intMax + 1,
+                    StartTime = startTime.ToLocalTime(),
+                    EndTime = endTime.ToLocalTime(),
+                    Subject = value.Subject
+                };
+                db.ScheduleEventDatas.InsertOnSubmit(appointment);
+                db.SubmitChanges();
+            }
+            if (param.action == "update" || (param.action == "batch" && param.changed != null)) // this block of code will execute while updating the appointment
+            {
+                var value = (param.action == "update") ? param.value : param.changed[0];
+                var filterData = db.ScheduleEventDatas.Where(c => c.Id == Convert.ToInt32(value.Id));
+                if (filterData.Count() > 0)
+                {
+                    DateTime startTime = Convert.ToDateTime(value.StartTime);
+                    DateTime endTime = Convert.ToDateTime(value.EndTime);
+                    ScheduleEventData appointment = db.ScheduleEventDatas.Single(A => A.Id == Convert.ToInt32(value.Id));
+                    appointment.StartTime = startTime.ToLocalTime();
+                    appointment.EndTime = endTime.ToLocalTime();
+                    appointment.Subject = value.Subject;
+                }
+                db.SubmitChanges();
+            }
+            if (param.action == "remove" || (param.action == "batch" && param.deleted != null)) // this block of code will execute while removing the appointment
+            {
+                if (param.action == "remove")
+                {
+                    int key = Convert.ToInt32(param.key);
+                    ScheduleEventData appointment = db.ScheduleEventDatas.Where(c => c.Id == key).FirstOrDefault();
+                    if (appointment != null) db.ScheduleEventDatas.DeleteOnSubmit(appointment);
+                }
+                else
+                {
+                    foreach (var apps in param.deleted)
+                    {
+                        ScheduleEventData appointment = db.ScheduleEventDatas.Where(c => c.Id == apps.Id).FirstOrDefault();
+                        if (apps != null) db.ScheduleEventDatas.DeleteOnSubmit(appointment);
+                    }
+                }
+                db.SubmitChanges();
+            }
+            var data = db.ScheduleEventDatas.ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }       
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/EditParams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScheduleSample.Models
+{
+    public class EditParams
+    {
+        public string key { get; set; }  // key value is appointment id value - while deleting the appointment it comes
+        public string action { get; set; } // action indicates the current action of the CRUD operation. Ex: while adding new appointment the action value coming as "insert"
+        public List<ScheduleEventData> added { get; set; } // added indicates the newly added appointment object details while modifying the recurrence events
+        public List<ScheduleEventData> changed { get; set; } // changed indicates the edited/updated appointment object details
+        public List<ScheduleEventData> deleted { get; set; } // deleted indicates the deleted appointment object details
+        public ScheduleEventData value { get; set; } // value indicates the newly added appointment object details
+    }
+}
